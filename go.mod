d3ec944e6e7cd75e781ee85816cb5e0762a32c55
Tom <tsenseless@gmail.com>
2026-01-17 22:46:12 -0500

    Update

--- go.mod
+++ go.mod
@@ -2,17 +2,31 @@ module github.com/ibp-network/ibp-geodns-agent
 
 go 1.24.2
 
-require (
-	github.com/ibp-network/ibp-geodns-libs v0.2.0
-	github.com/nats-io/nats.go v1.48.0
-)
+require github.com/ibp-network/ibp-geodns-libs v0.2.0
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.18.3 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/nats-io/nats.go v1.48.0 // indirect
 	github.com/nats-io/nkeys v0.4.12 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/oschwald/maxminddb-golang v1.13.1 // indirect
+	github.com/rs/zerolog v1.34.0 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	go.mau.fi/util v0.9.1 // indirect
 	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
+	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	maunium.net/go/mautrix v0.25.1 // indirect
 )
 
 replace github.com/ibp-network/ibp-geodns-libs => ../ibp-geodns-libs
--- src/nats/client.go
+++ src/nats/client.go
@@ -1,81 +1,87 @@
 package nats
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/ibp-network/ibp-geodns-agent/src/config"
 	"github.com/ibp-network/ibp-geodns-agent/src/logging"
+	libsnats "github.com/ibp-network/ibp-geodns-libs/nats"
 	natsgo "github.com/nats-io/nats.go"
 )
 
-// Client wraps the NATS connection
+// Client wraps the NATS connection from ibp-geodns-libs
+// The libs package uses a global connection pattern, so this wrapper
+// provides a cleaner interface for the agent
 type Client struct {
-	conn *natsgo.Conn
-	cfg  config.NatsConfig
+	cfg config.NatsConfig
 }
 
 // NewClient creates a new NATS client using ibp-geodns-libs
+// Note: The libs package uses a global connection, so Connect() must be called
+// before using other functions. This wrapper manages that connection.
 func NewClient(cfg config.NatsConfig) (*Client, error) {
-	// Use ibp-geodns-libs for NATS connectivity
-	// For now, this is a placeholder that would use the actual libs implementation
+	// The ibp-geodns-libs/nats package uses a global connection pattern
+	// We need to ensure Connect() is called, which uses the global NC variable
+	// The connection is configured through the libs package's internal config
 	
-	opts := []natsgo.Option{
-		natsgo.Name(cfg.NodeID),
-		natsgo.ReconnectWait(2 * time.Second),
-		natsgo.MaxReconnects(-1),
-		natsgo.DisconnectErrHandler(func(nc *natsgo.Conn, err error) {
-			if err != nil {
-				logging.Warn("NATS disconnected", "error", err)
-			}
-		}),
-		natsgo.ReconnectHandler(func(nc *natsgo.Conn) {
-			logging.Info("NATS reconnected", "url", nc.ConnectedUrl())
-		}),
+	// For now, we'll use the libs Connect() function
+	// The actual connection details should be set up in the libs package
+	if err := libsnats.Connect(); err != nil {
+		return nil, err
 	}
 
-	// Add authentication if provided
-	if cfg.User != "" && cfg.Pass != "" {
-		opts = append(opts, natsgo.UserInfo(cfg.User, cfg.Pass))
-	}
-
-	conn, err := natsgo.Connect(cfg.Url, opts...)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to NATS: %w", err)
-	}
-
-	logging.Info("Connected to NATS", "url", cfg.Url, "nodeID", cfg.NodeID)
+	logging.Info("Connected to NATS via ibp-geodns-libs", "nodeID", cfg.NodeID)
 
 	return &Client{
-		conn: conn,
-		cfg:  cfg,
+		cfg: cfg,
 	}, nil
 }
 
-// Publish publishes a message to a subject
+// Publish publishes a message to a subject using ibp-geodns-libs
 func (c *Client) Publish(subject string, data []byte) error {
-	return c.conn.Publish(subject, data)
+	return libsnats.Publish(subject, data)
 }
 
-// Subscribe subscribes to a subject
+// Subscribe subscribes to a subject using ibp-geodns-libs
 func (c *Client) Subscribe(subject string, handler natsgo.MsgHandler) (*natsgo.Subscription, error) {
-	return c.conn.Subscribe(subject, handler)
+	// Convert natsgo.MsgHandler to the libs callback format
+	cb := func(msg *natsgo.Msg) {
+		handler(msg)
+	}
+	return libsnats.Subscribe(subject, cb)
 }
 
 // QueueSubscribe subscribes to a subject with queue group
+// Note: The libs package may not have QueueSubscribe, so we use the underlying connection
 func (c *Client) QueueSubscribe(subject, queue string, handler natsgo.MsgHandler) (*natsgo.Subscription, error) {
-	return c.conn.QueueSubscribe(subject, queue, handler)
+	conn := libsnats.GetConnection()
+	if conn == nil {
+		return nil, libsnats.Connect()
+	}
+	cb := func(msg *natsgo.Msg) {
+		handler(msg)
+	}
+	return conn.QueueSubscribe(subject, queue, cb)
 }
 
 // Close closes the NATS connection
 func (c *Client) Close() error {
-	if c.conn != nil {
-		c.conn.Close()
-	}
+	libsnats.Disconnect()
 	return nil
 }
 
 // IsConnected returns whether the client is connected
 func (c *Client) IsConnected() bool {
-	return c.conn != nil && c.conn.IsConnected()
+	conn := libsnats.GetConnection()
+	return conn != nil && conn.IsConnected()
+}
+
+// GetConnection returns the underlying NATS connection for advanced usage
+func (c *Client) GetConnection() *natsgo.Conn {
+	return libsnats.GetConnection()
+}
+
+// Request sends a request and waits for a response
+func (c *Client) Request(subject string, data []byte, timeout time.Duration) (*natsgo.Msg, error) {
+	return libsnats.Request(subject, data, timeout)
 }
